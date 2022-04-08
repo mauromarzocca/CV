1087d09ec8ecc6cc99ad891f8421b2be2863083a
mauromarzocca <57107500+mauromarzocca@users.noreply.github.com>
2022-04-08 17:53:28 +0200

    Clean project

--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -56,44 +56,4 @@
     document.addEventListener("scroll", collapseNavbar);
   }
 
-  function verificaEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-  
-  $(function() {
-    $("#contatti").validate ({
-    rules:{
-    'nome':{
-    required: true,
-    minlength: 3
-    },
-    'email':{
-    required: true,
-    email: true
-    },
-    'messaggio':{
-    required: true,
-    minlength: 10
-    }
-    },
-    messages:{
-    'nome':{
-    required: "Il campo nome è obbligatorio!",
-    minlength: "Inserisci un nome di almeno 3 lettere!"
-    },
-    'email':{
-    required: "L' email è obbligatoria!",
-    email: "L'Email inserita non è valida!"
-    },
-    'messaggio':{
-    required: "Il campo messaggio è obbligatorio!",
-    minlength: "Insersci un messaggio di almeno 10 caratteri!"
-    }
-    },
-    submitHandler : function(form) {
-    form.submit();
-    }
-    });
-    });
 })(); // End of use strict
